0c5733be781705314e70fad6cd03bf2c27efdd04
ragOP <najmi.raghib@gmail.com>
2024-05-03 00:48:07 +0530

    css

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import Footer from "../Component/Footer";
 import Media from "../Component/Media";
 import Diversity from "../Component/Diversity";
+import Carrer from "../Component/Carrer";
 
 const Homepage = () => {
   return (
     <>
-      <Diversity/>
+      <Carrer />
+
+      {/* <Diversity/> */}
       {/* <Media/> */}
       {/* <Footer /> */}
     </>
